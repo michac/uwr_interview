7a41af476c74189eeb58180529c82acf8eae2b5c
Michael Christiansen <mchristiansen@unwiredrevolution.com>
2014-02-12 15:31:43 -0700

    Creation of interview project.

---
+++ InterviewProject/IOneWayLinkedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewProject
+{
+    public interface IOneWayLinkedList
+    {
+        IOneWayLinkedListNode First { get; }
+
+        void AddLast(int value);
+
+        void AddFirst(int value);
+
+        List<int> ToList();
+
+        int Count { get; }
+    }
+}
---
+++ InterviewProject/IOneWayLinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewProject
+{
+    interface IOneWayLinkedListNode
+    {
+        IOneWayLinkedList List { get; }
+
+        IOneWayLinkedListNode Next { get; }
+    }
+}
