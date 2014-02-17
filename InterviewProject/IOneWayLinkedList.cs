ffc1df3cb863c0bfe8775f31c109ffaee4a12db0
Michael Christiansen <mchristiansen@unwiredrev.com>
2014-02-17 14:49:57 -0700

    Remoted the IOneWayLinkedListNode interface to be slightly less confusing. Added some comments, renamed some methods, and generally tried to reduce noise.

--- InterviewProject/IOneWayLinkedList.cs
+++ InterviewProject/IOneWayLinkedList.cs
@@ -5,16 +5,36 @@ using System.Text;
 
 namespace InterviewProject
 {
+    /// <summary>
+    /// Interface for a one-way forward linked list. Implementations should
+    /// only hold a reference to the first node in the list, then each node has
+    /// a reference to the next node.
+    /// </summary>
     public interface IOneWayLinkedList
     {
-        IOneWayLinkedListNode First { get; }
+        OneWayLinkedListNode First { get; }
 
-        void AddLast(int value);
+        /// <summary>
+        /// Insert a value to the end of the list.
+        /// </summary>
+        /// <param name="value"></param>
+        void InsertLast(int value);
 
-        void AddFirst(int value);
+        /// <summary>
+        /// Insert a value to the beginning of the list.
+        /// </summary>
+        /// <param name="value"></param>
+        void InsertFirst(int value);
 
-        List<int> ToList();
+        /// <summary>
+        /// Convert the linked list to a generic .NET List.
+        /// </summary>
+        /// <returns></returns>
+        List<int> ToFrameworkList();
 
+        /// <summary>
+        /// Return a count of the number of items in the list.
+        /// </summary>
         int Count { get; }
     }
 }
---
+++ InterviewProject/OneWayLinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewProject
+{
+    public class OneWayLinkedListNode
+    {
+        public OneWayLinkedListNode(int value, OneWayLinkedListNode next)
+        {
+            Value = value;
+            Next = next;
+        }
+
+        public OneWayLinkedListNode Next { get; private set; }
+
+        public int Value { get; private set;  }
+    }
+}
